72e4d785635779aa65d6f8de90f9287f13544f2c
priya-chatwani <priyac5@stanford.edu>
2020-02-18 19:53:14 -0800

    attendees via firebase
    attendees via firebase

--- YPOApp/screens/Attendees.js
+++ YPOApp/screens/Attendees.js
@@ -1,6 +1,7 @@
 import * as WebBrowser from 'expo-web-browser';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { SearchBar } from 'react-native-elements';
+import * as firebase from 'firebase';
 import {
   Image,
   Platform,
@@ -11,14 +12,26 @@ import {
   View,
 } from 'react-native';
 
+export default function Attendees() {
 
-export default function Attendees(){
-	return (
-		<View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+  const [attendeeList, setattendeeList] = useState([]);
+  var ref = firebase.database().ref('Attendees');
+
+  useEffect(() => {
+    ref.once('value', function(snapshot) {
+      return snapshot.forEach(function(attendeeSnapshot) {
+        setattendeeList(attendeeList => [...attendeeList, attendeeSnapshot.val()])
+      });
+    });
+  });  
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'flex-start' }}>
       <SearchBar
-         showLoading
-         platform="ios"
-         placeholder='Search' />
-    	</View>
-	);
-}
+        showLoading
+        platform="ios"
+        placeholder='Search' 
+      />
+    </View>
+  );
+};
