24fc886500d8191777d9aed6a49d9d8993c7b223
jtopete135 <35283607+jtopete135@users.noreply.github.com>
2020-03-04 00:15:10 -0800

    Added Contact, Website, and Phone to Org Bios

--- YPOApp/screens/OrgBio.js
+++ YPOApp/screens/OrgBio.js
@@ -13,16 +13,34 @@ import {
 
 import * as firebase from 'firebase';
 
+function infoTitle(title, info){
+  return(
+    <View style={styles.infoItem}>
+      <Text style={styles.infoTitle}>
+        {title}
+      </Text>
+      <Text style={{fontSize: 16}}>
+        {info}
+      </Text>
+    </View>
+  )
+}
+
 export default function OrgBio(Props){
   const org = Props.navigation.getParam('org')
   return(
     <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.org}>
-        {org.Organization}
-      </Text>
-      <Text style={styles.description}>
-        {org.Description}
-      </Text>
+      <View style = {styles.bio}>
+        <Text style={styles.org}>
+          {org.Organization}
+        </Text>
+        <Text style={styles.description}>
+          {org.Description}
+        </Text>
+      </View>
+      {org.Contact.length != 0 ? infoTitle("Contact: ", org.Contact) : null}
+      {org.Contact.length != 0 ? infoTitle("Website: ", org.Website) : null}
+      {org.Phone.length != 0 ? infoTitle("Phone: ", org.Phone) : null}
     </ScrollView>
   );
 }
@@ -31,7 +49,10 @@ const styles = StyleSheet.create({
   container: {
     padding: 10,
     flexDirection: 'column',
+  },
+  bio: {
     alignItems: 'center',
+    marginBottom: 10,
   },
   org: {
     fontSize: 30,
@@ -40,5 +61,13 @@ const styles = StyleSheet.create({
   description: {
     fontSize: 16,
     marginTop: 15,
-  }
+  },
+  infoItem: {
+    flexDirection: 'row',
+    paddingBottom: 5,
+  },
+  infoTitle: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
 });
